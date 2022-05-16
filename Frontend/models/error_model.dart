057bac051819e444f9d90341a427e9bd33cf36d2
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-05-16 09:56:37 +0300

    Add files via upload

---
+++ Frontend/models/error_model.dart
@@ -0,0 +1,10 @@
+class ErrorMessage {
+  String message;
+  ErrorMessage({required this.message});
+
+  factory ErrorMessage.fromJson(Map<String, dynamic> json) {
+    return ErrorMessage(
+      message: json['message'] as String,
+    );
+  }
+}

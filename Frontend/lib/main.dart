336b099fe0ce4f9217748607ca49ade9437cf988
birehan <91595957+birehan@users.noreply.github.com>
2022-06-05 23:43:59 +0300

    Update main.dart

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,28 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:project/home.dart';
-import 'loginpage.dart';
-import 'signup_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:please/application/answer/answer_bloc.dart';
+import 'package:please/application/auth/authentication_bloc.dart';
+import 'package:please/application/question/question_bloc.dart';
+import 'package:please/infrastructure/answer/answer_remote_data_provider.dart';
+import 'package:please/infrastructure/answer/answer_repository_impl.dart';
+import 'package:please/infrastructure/auth/shared_preference_repository.dart';
+import 'package:please/infrastructure/question/question_repository_impl.dart';
+import 'package:please/infrastructure/question/question_remote_data_provider.dart';
+
+import 'application/auth/sign_up_form/sign_up_form_bloc.dart';
+import 'infrastructure/auth/user_remote__data_provider.dart';
+import 'infrastructure/auth/user_repository_impl.dart';
+import 'presentation/routes/routes.dart';
 
 void main() {
-  return runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeScreen(),
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/login': (context) => const LogInScreen(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/register': (context) => const SignUpPage(),
-      },
-      title: "Log in Page",
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthenticationBloc>(
+          create: (BuildContext context) => AuthenticationBloc(SharedPreferenceRepository()),
+        ),
+        BlocProvider<QuestionBloc>(
+          create: (BuildContext context) => QuestionBloc(
+              QuestionRepositoryImpl(QuestionRemoteDataProvider())),
+        ),
+        BlocProvider<AnswerBloc>(
+          create: (BuildContext context) => AnswerBloc(
+              answerRepository:
+                  AnswerRepositoryImpl(AnswerRemoteDataProvider())),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: router.routeInformationParser,
+        routerDelegate: router.routerDelegate,
+        title: 'AAiT Stackover Flow',
+      ),
     );
   }
 }

a700a156df2d24d1d87de9e36309b1f6387b0784
Tsedalemariam Asrate <96542324+beyon-d@users.noreply.github.com>
2022-06-06 07:40:22 +0300

    Update widget_test.dart

--- Frontend/test/widget_test.dart
+++ Frontend/test/widget_test.dart
@@ -5,26 +5,39 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// import 'dart:convert';
+// import 'dart:js';
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:please/main.dart';
+//
+import 'package:please/presentation/answer/add_answer_screen.dart';
+import 'package:please/presentation/question/questions_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('answer page widget test', (WidgetTester tester) async {
+    final addfield = find.byWidget(TextFormField());
+    final addbutton = find.byWidget(ElevatedButton(onPressed:(){} , child: Text("submit"),));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpWidget(const AddAnswerScreen(questionId: "SE"));
+    await tester.enterText(addfield, "we could enter an answer like this");
+    await tester.tap(addbutton);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(addfield, findsNWidgets(2));
+    expect(find.text("we could enter an answer like this"), findsWidgets);
+  });
+}
+
+void mainn() {
+  testWidgets("question page tester", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: QuestionScreen()));
+    await tester.drag(find.byType(ListView), Offset(0, -100));
+    await tester.pump();
+
+    final ListTileFinder = find.byType(ListTile);
+    expect(ListTileFinder, findsWidgets);
   });
 }
